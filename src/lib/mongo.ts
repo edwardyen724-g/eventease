671bc6ea10aaff6425d5c9d79399efd52f17b65a
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:35:06 -0800

    feat: add src/lib/mongo.ts

---
+++ src/lib/mongo.ts
@@ -0,0 +1,35 @@
+import mongoose, { ConnectOptions } from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts: ConnectOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
