e372fa46f7888ce48bf4d5fc1fb42814ba4032e9
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:35:19 -0800

    feat: add missing module src/lib/dbConnect.ts

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+export default dbConnect;
