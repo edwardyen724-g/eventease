f35b3f4ca5efabbd88db20ccc3b82d12c62e18f0
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:34:49 -0800

    feat: add tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,19 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { daisyUI } = require('daisyui');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', ...fontFamily.sans],
-      },
       colors: {
-        primary: '#4F46E5', // Indigo 600
-        secondary: '#FBBF24', // Yellow 400
-        accent: '#3B82F6', // Blue 500
-        background: '#F9FAFB', // Gray 200
-        surface: '#FFFFFF', // White
-        error: '#EF4444', // Red 600
-      },
-      spacing: {
-        '72': '18rem',
-        '84': '21rem',
-        '96': '24rem',
-      },
-      screens: {
-        '3xl': '1792px',
+        primary: '#1E3A8A', // Example primary color
+        secondary: '#FBBF24', // Example secondary color
       },
     },
   },
-  plugins: [],
+  plugins: [daisyUI],
 };
